aba18260dff03dc2f7c0ec5a21d85234981dd5ee
Jefferson Uchoa Ponte <jefponte@unilab.edu.br>
2016-08-23 23:07:39 -0300

    Buble Sorte

--- python/main.py
+++ python/main.py
@@ -7,9 +7,6 @@ def selection(lista):
         auxiliar = lista[i]
         lista[i] = lista[menor]
         lista[menor] = auxiliar
-		
-
-
 def insertion(lista):
 	for i in range(1, len(lista)):
 		eleito = lista[i]
@@ -18,9 +15,15 @@ def insertion(lista):
 			lista[j] = lista[j-1]
 			j = j - 1
 		lista[j] = eleito
-			
+def bubble(lista):
+	for i in range(0, len(lista)):
+		for j in range(len(lista)-1, i, -1):
+			if lista[j] < lista[j-1]:
+				lista[j], lista[j-1] = lista[j-1], lista[j]
+		
+		
 lista = [5, 3, 2, 4, 1]
 print lista
-insertion(lista)
+bubble(lista)
 print "------------------"
 print lista
